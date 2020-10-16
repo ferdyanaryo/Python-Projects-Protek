180e1a683b4fd993b4931056384283dba1acacbd
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2020-10-16 22:07:01 +0700

    update

---
+++ Praktikum 04/latihan05.py
@@ -0,0 +1,8 @@
+jl  = int(input("Jumlah mahasiswa laki-laki : "))
+jp  = int(input("Jumlah mahasiswa perempuan : "))
+
+pl  = jl//10
+pp  = jp//10
+
+print("Laki-laki     :","*"*pl,"(",jl,")")
+print("Perempuan     :","*"*pp,"(",jp,")")
