335a157bb073bb39423b0a988633004f9f92ab3d
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2020-10-16 21:47:26 +0700

    update

--- Praktikum 04/latihan03.py
+++ Praktikum 04/latihan03.py
@@ -1,3 +1,9 @@
 j   = int(input("Input Jarak tempuh (km) ..."))
 bbm = j/12
 print("Jumlah BBM yang diperlukan sebanyak",bbm,"liter")
+
+fulltank = 50
+fill     = bbm//50
+
+print("Pak Budi harus mengisi bbm sampai penuh sebanyak",fill,"kali")
+
