6c8613ef7a30cb996e8c553fe3cce9550756ccd2
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2020-10-22 10:22:08 +0700

    update

---
+++ Praktikum 05/latihan02.py
@@ -0,0 +1,19 @@
+nindo   = int(input("MasukKan nilai Bhs Indonesia   : "))
+nmtk    = int(input("Masukkan nilai Matematika      : "))
+nipa    = int(input("Masukkan nilai IPA             : "))
+
+if (0 <= nmtk <= 100) and (0 <= nindo <= 100) and (0 <= nipa <= 100):
+    if (nmtk > 70):
+        if (nindo >= 60) and (nipa >= 60):
+            status = "LULUS"
+        else:
+            status = "TIDAK LULUS"
+    else:
+        status = "TIDAK LULUS"
+    
+    print("Status Kelulusan               :",status)
+
+else:
+    print("")
+    print("Maaf input ada yang tidak valid")
+    print("")
