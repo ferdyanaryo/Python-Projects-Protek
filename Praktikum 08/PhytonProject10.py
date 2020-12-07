2325e6a178d3e97dc527e0fe906499aa7138b10d
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2020-12-07 21:24:26 +0700

    eror

---
+++ Praktikum 08/PhytonProject10.py
@@ -0,0 +1,39 @@
+buah = {'apel' : 5000, 
+        'jeruk' : 8500, 
+        'mangga' : 7800,
+        'duku' : 6500}
+
+#print('Daftar harga buah :')
+
+#for x,y in buah.items():
+#    print(x,': Rp',y)
+
+x= []
+
+while True:
+    #try:
+    print('')
+    pilih = str(input("Nama buah yang dibeli = "))
+    if pilih == 'apel' or pilih == 'jeruk' or pilih == 'mangga' or pilih == 'duku':
+        
+        try:
+            jumlh = int(input("Berapa  Kg            = "))
+            z = buah.get(pilih,0)*jumlh
+            x.append(z)
+        except ValueError:
+            print("input harus angka")
+            print('')
+            continue
+        
+        print('')
+        lagii = str(input('Beli buah yang lain (y/n) ? '))
+        if lagii == 'n':
+            print('------------------------')
+            print('Total harga           =',sum(x))
+            break
+
+    else:
+        print("Nama buah tidak ditemukan")
+        print('')
+    #except         
+        
--- Praktikum 08/PhytonProject9.py
+++ Praktikum 08/PhytonProject9.py
@@ -9,13 +9,19 @@ for x,y in buah.items():
     print(x,': Rp',y)
 
 while True:
-
+    #try:
     pilih = str(input("Nama buah yang dibeli = "))
-    while True:
-        try:
-            jumlh = int(input("Berapa                = "))
-            print('------------------------')
-            print('Total harga           =',buah.get(pilih,0)*jumlh)
-        except ValueError:
-            print('tidak valid')
-            continue
+    if pilih == 'apel' or pilih == 'jeruk' or pilih == 'mangga' or pilih == 'duku':
+        while True:
+            try:
+                jumlh = int(input("Berapa                = "))
+                print('------------------------')
+                print('Total harga           =',buah.get(pilih,0)*jumlh)
+                break
+            except ValueError:
+                print("input harus angka")
+        break
+    else:
+        print("Nama buah tidak ditemukan")
+    #except         
+        
