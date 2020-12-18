a5991244f1d5be79a40da9bee5b91efd1b842267
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2020-12-18 23:37:41 +0700

    update

---
+++ Praktikum 09/latihan3.py
@@ -0,0 +1,18 @@
+def bintang(n):
+    if (n % 2 == 1): #n harus bilangan ganjil
+        n2 = int((n-1)/2)
+        n3 = n2+1
+
+        for i in range(0,n2):
+            x = (i*2+1) * '*'
+            print(x.center(n))
+        
+        for i in reversed(range(0,n3)):
+            x = (i*2+1) * '*'
+            print(x.center(n))
+    
+    else:
+        print('n bukan ganjil')
+            
+
+bintang(9)
