3c558fed4952b3b14b741c16863901fb717bfc96
aryoferdyan <63910862+aryoferdyan@users.noreply.github.com>
2020-12-21 09:34:02 +0700

    update

--- Praktikum 09/latihan4.py
+++ Praktikum 09/latihan4.py
@@ -5,12 +5,11 @@ def shuffleString(x,n):
     hsl = []
     i   = 0
     while i < n:
-        print(i)
+        print(i+1)
         ack = ''.join(random.sample(x,len(x)))
-        if ack not in hsl:
+        if (ack not in hsl):
             hsl.append(ack)
             i   += 1
-       
     print(hsl)
 
-shuffleString('abc',7)
+shuffleString('abc',6)
